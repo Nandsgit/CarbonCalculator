aa12aa87038d200c0ea1adfb77325d03e25b3d69
Nandsgit <70517841+Nandsgit@users.noreply.github.com>
2021-10-23 22:17:29 +0545

    Add files via upload

---
+++ src/components/home/cardscomp/carditem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function carditem(props) {
+    return (
+        <>
+        <li className ='cards_item'>
+            <Link className = 'cards__item__link' to = {props.path}>
+                <figure className ='cards__item__pic-wrap' data-catergory={props.label}>
+                    <img src={props.src} alt='Test Image' className = 'cards__item__img' />
+                </figure>
+                <div className='cards__item__info'>
+                    <h5 className='cards__item__text'> {props.text}  </h5>
+                </div>
+            </Link>
+        </li>
+        </>
+    );
+}
+
+export default carditem
